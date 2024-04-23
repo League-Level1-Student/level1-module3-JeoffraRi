936913abd66feaa37f07b50c11071cdfb351f9b9
AmtrakFTW <88908613+JeoffraRi@users.noreply.github.com>
2024-04-22 18:27:18 -0700

    Frogger.java (IP)
    got the cars to move, will work on collision next week
    :sleeping:

--- src/_06_frogger/Frogger.java
+++ src/_06_frogger/Frogger.java
@@ -3,11 +3,15 @@ package _06_frogger;
 import processing.core.PApplet;
 
 public class Frogger extends PApplet {
-    static final int WIDTH = 600;
+    static final int WIDTH = 800;
     static final int HEIGHT = 400;
     int x = 400;
     int y = 550;
-    
+    int carX = 150;
+    int carY = 270;
+    Car carOne;
+    Car carTwo;
+    Car carThree;
     @Override
     public void settings() {
         size(800, 600);
@@ -15,14 +19,16 @@ public class Frogger extends PApplet {
 
     @Override
     public void setup() {
-    	Car carOne = new Car(400, 550, 20, 30);
+    carOne = new Car(200, 300, 20, 30);
+    carTwo = new Car(600, 200, 20, 30);
+    carThree = new Car(400, 100, 20, 30);
     }
 
     @Override
     public void draw() {
     	background(100,100,100);
     	fill(0, 100, 0);
-    	ellipse(x, y, 50, 50);
+    	ellipse(x, y, 40, 40);
     	if(x<0) {
     		x = 780;
     	}else if(x>800) {
@@ -34,8 +40,22 @@ public class Frogger extends PApplet {
     	}
     	
     	fill(255,135,0);
-        rect(x , y,  100, 50);
-    	
+        rect(carOne.x, carOne.y,  100, 50);
+        rect(carTwo.x, carTwo.y,  100, 50);
+        rect(carThree.x, carThree.y, 100, 50);
+        
+        carOne.x+=5;
+        if(carOne.x>830) {
+        	carOne.x=-30;
+        }
+        carTwo.x-=5;
+        if(carTwo.x<-30) {
+        	carTwo.x=830;
+        }
+        carThree.x+=5;
+        if(carThree.x>830) {
+        	carThree.x=-30;
+        }
     }
     
     public void keyPressed()
